60949de641bdc8de8051e1f86577c63c6e7dd250
wjdgoruds2 <rnldudns989@naver.com>
2022-02-28 19:39:39 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-	printf("6조");
+	cout<<"주차관리시스템";
+	cout<<"--------------------------------";
+	int num;
+	cout<<"선택하실 메뉴 번호를 입력하세요";
+	cin >> num; 
+	
+	cout<<"1.입차\n";
+	cout<<"2.출차\n";
+	cout<<"3.내 주차이력\n";
+	cout<<"4.조회\n";
+	cout<<"5.회원등록\n";
+	cout<<"6.위치확인\n";
 
 }
