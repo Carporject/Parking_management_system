14b9e48fdfe27bcb6b9929d2b2313762a5ae6f48
hyunbeen99 <gusqls1124@gmail.com>
2022-03-01 19:18:22 +0900

    Fix the error loadexit, add perDay func

--- Admin.cpp
+++ Admin.cpp
@@ -1,80 +1,118 @@
 #include "include/Admin.h"
-
+#include <iomanip>
 
 void Admin::loadExit(){
 
-
-    vector<string> getoneline_;
-
     FILE *inputFile = NULL;
-
     inputFile = fopen("./total.txt", "r");
-    
-    int lineCount = 1;
 
     if(inputFile != NULL){
         char buffer[256];
 
         while(!feof(inputFile)){
+            //한줄 씩 입력 받음
             fgets(buffer, sizeof(buffer), inputFile);
 
             char *ptr = strtok(buffer, ",");
 
-            while(ptr != NULL){
+            while(ptr){
                 string str(ptr);
-                getoneline_.push_back(str);
-
-                ptr = strtok(NULL, " ");
-                vector<string> getoneline_;
+                memlist_[lineCount].push_back(str);
+                ptr = strtok(NULL, ",");
             }
+
+            lineCount++;
         }
-        memlist_.push_back(getoneline_);
-        lineCount++;
         fclose(inputFile);
 
     }else {
         printf("입력 파일 존재 X. \n");
     }
 
-    //%%%%%%file load 된 것 확인%%%%%
-    /*for(int i=0; i<memlist_.size(); i++){
-        for(int j=0; j<getoneline_.size(); j++){
-            cout << i  << " ====" << j  << " " ;
-            cout << memlist_[i][j] << '\n';
-        }
-    }*/
+}
 
-    int j=0;
+void Admin::print(){
 
-    while(j <= getoneline_.size()+11){
-        car_num.push_back(memlist_[0][j]);
-        phone_num.push_back(memlist_[0][j+1]);
-        j += 11;
+    cout << "===================================================================="<<'\n';
+    cout << "  차량번호      전화번호         입차일           출차일       요금"<<'\n';
+    cout << "===================================================================="<<'\n';
+    for(int i=0; i<lineCount; i++){
+        for(int j=0; j<PINFO; j++){
+            cout << this->memlist_[i][j] << " ";
+        }cout << "\n";
     }
-
-    for(auto a : car_num){
-        cout << a << " ";
-    }cout << '\n';
-    for(auto a : phone_num){
-        cout << a << " ";
-    }cout << '\n';
-
 
 }
 
 void Admin::perDay(){
-    //차량번호, 전화번호, 입차일, 출차일, 정산요금
-    vector<vector<string>> &mlist = this->memlist_;
+
+    int month, day;
+
+    string exit_date = this->memlist_[lineCount-1][3];  //출차일 기준으로 7일 계산
+
+    // 문자열 parsing 
+    string m = "";
+    string d = "";
+
+    for(int i=1; i<11; i++){ 
+        if(i>=6 && i<=7){
+            m += exit_date[i];
+        }else if(i>=9 && i<=10){
+            d += exit_date[i];
+        }
+    }
+
+    month = stoi(m);
+    day = stoi(d);
 
 
+    // 마지막 출차일을 일 단위로 변환 
+    int lastday = month * change_month[month] + day;
 
+    // 가장 최근 이용자의 요금으로 total cost 초기화
+    total_cost = stoi(this->memlist_[lineCount-1][4]);
 
+    // 리스트 순회 하며 날짜 변환 -> 일단위로 변환 -> 차가 7일 이내면 요금 더함
+    for(int k=lineCount-2; k>=0; k--){
 
+        int diffday, diffmonth;
+        int mon2day_ = 0;
+
+        string diff = this->memlist_[k][3];
+
+        string dm = "";
+        string dd = "";
+
+        for(int i=1; i<11; i++){ 
+            if(i>=6 && i<=7){
+                dm += diff[i];
+            }else if(i>=9 && i<=10){
+                dd += diff[i];
+            }
+        }
+
+        diffday = stoi(dd);
+        diffmonth= stoi(dm);
+
+        mon2day_ = diffmonth * change_month[diffmonth] + diffday;
+
+        if( (lastday - mon2day_) <=7 && (lastday - mon2day_) >= 0){
+            string cost_ = this->memlist_[k][4];
+            total_cost += stoi(cost_);
+
+        }else break;
+    }
+
+    cout << " 최근 7일간 정산 금액 ==> " << total_cost << endl;
 
 }
 
+
 void Admin::perMonth(){
-    cout << " still upgrading ............... " << endl;
+    cout << "stilling upgrading........" << endl;
+
+
+
 }
 
 
@@ -89,28 +127,31 @@ int main(){
 
     cout << "주차 관리 시스템 관리자 모드" << '\n';
 
-    cout << "1. 전체 회원 리스트 " << '\n';
-    cout << "2. 일별 정산 요금 조회" << '\n';
-    cout << "3. 월별 정산 요금 조회" << '\n';
+    while(select == 0){
+        cout << "1. 전체 회원 리스트 " << '\n';
+        cout << "2. 일별 정산 요금 조회" << '\n';
+        cout << "3. 월별 정산 요금 조회" << '\n';
 
 
-    cin >> select;
+        cin >> select;
 
-    switch(select){
-        case 1:
-            a->loadExit();
-            break;
-        case 2:
-            a->perDay();
-            break;
-        case 3:
-            a->perMonth();
-            break;
-        case 0:
-            break;
-            return 0;
-        default:
-            cout << "잘못된 입력 입니다. " << select << endl;
+        switch(select){
+            case 1:
+                a->print();
+                continue;
+            case 2:
+                a->perDay();
+                continue;
+            case 3:
+                a->perMonth();
+                continue;
+            case 0:
+                break;
+            default:
+                cout << "잘못된 입력 입니다. " << select << endl;
+        }
     }
+
+    return 0;
 
 }
--- include/Admin.h
+++ include/Admin.h
@@ -6,24 +6,35 @@
 #include <time.h>
 using namespace std;
 
+#define PINFO 5 // 차량번호, 전화번호, 입차일, 출차일, 요금
+
 class Admin{
 private:
     vector<string> car_num;
     vector<string> phone_num;
     vector<string> cost;
-    vector<string> enter_date;
-    vector<string> exit_date;
+
+    string enter_date;
+    string exit_date;
 
     int is_mem;
     int total_cost;
 
+protected:
+    int lineCount = 0;
+    vector<string> memlist_[INT16_MAX];
 
 public:
 
-    vector<vector<string>> memlist_;
+    Admin() {
+        loadExit();
+    }
 
     void loadExit();
+    void print();
     void perDay();
     void perMonth();
+
+    int change_month[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
 };
